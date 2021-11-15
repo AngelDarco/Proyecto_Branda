b40cf58eea766b9131b0564a3bdc0a9c60f23122
eme92 <angel92-eme@hotmail.com>
2021-11-15 03:15:23 -0500

    Js recuperado

---
+++ js/carro.js
@@ -0,0 +1,25 @@
+export default function carro(){
+
+    const productos = document.querySelectorAll('.product__icon');
+    let array = [];
+
+        productos.forEach(item=>{
+                item.addEventListener('click', (e)=>{
+                producto = item.parentNode;
+                producto.classList.toggle('agregado');
+
+                if(producto.classList.contains('agregado')){
+
+                    array.push(producto);
+                    console.log('agregado');
+                }else{
+
+
+
+                    console.log('Eliminado');
+                }
+
+            });
+        });
+
+}
