55cf329deb9f74a633261551a5d220c260fc1041
Angel Darco <angel92-eme@hotmail.com>
2023-03-13 01:42:43 -0500

    webpack modules add

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,95 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const path = require('path')
 
 module.exports = {
   mode: 'production',
-  entry: './index.html',
+  entry: {
+    app: './app.js',
+    cardsButtons: './js/cardsButtons.js',
+    formulario: './js/formulario.js',
+    shoppingCar: './js/shoppingCar.js',
+    slider: './js/slider.js',
+    userName: './js/userName.js'
+
+  },
   output: {
-    filename: 'main.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+      name: 'common'
+    }
+    // splitChunks: false
   },
   module: {
     rules: [
       {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader']
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
       },
+      // {
+      //   test: /\.css$/i,
+      //   use: [
+      //     MiniCssExtractPlugin.loader,
+      //     'css-loader']
+      // },
       {
         test: /\.html$/i,
         loader: 'html-loader'
+      },
+      {
+        test: /css\/card\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader'
+        ]
+      },
+      {
+        test: /css\/estilos\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader'
+        ]
+      },
+      {
+        test: /css\/productos\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader'
+        ]
+      },
+      {
+        test: /css\/registro\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader'
+        ]
+      },
+      {
+        test: /css\/slider\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader'
+        ]
       }
     ]
-  }
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[name].css'
+    }),
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      filename: 'index.html',
+      minify: true
+    })
+  ]
 }
