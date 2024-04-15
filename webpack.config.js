65f8cfe69a59728f8fb516f311a34cd26f5dad09
angeldarco <angel92-eme@hotmail.com>
2024-04-15 01:22:13 -0500

    :racehorse: webpack config updated

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,17 @@
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-const path = require('path')
+/* eslint-disable no-undef */
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const path = require("path");
 
 module.exports = {
-  mode: 'production',
+  mode: "production",
   entry: {
-    app: './app.js',
-    cardsButtons: './js/cardsButtons.js',
-    formulario: './js/formulario.js',
-    shoppingCar: './js/shoppingCar.js',
-    slider: './js/slider.js',
-    userName: './js/userName.js'
-
+    app: "./app.js",
   },
   output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  optimization: {
-    splitChunks: {
-      chunks: 'all',
-      name: 'common'
-    }
-    // splitChunks: false
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist"),
   },
   module: {
     rules: [
@@ -31,65 +19,90 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader'
-        }
+          loader: "babel-loader",
+        },
       },
-      // {
-      //   test: /\.css$/i,
-      //   use: [
-      //     MiniCssExtractPlugin.loader,
-      //     'css-loader']
-      // },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
       {
         test: /\.html$/i,
-        loader: 'html-loader'
+        loader: "html-loader",
       },
-      {
-        test: /css\/card\.css$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader'
-        ]
-      },
-      {
-        test: /css\/estilos\.css$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader'
-        ]
-      },
-      {
-        test: /css\/productos\.css$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader'
-        ]
-      },
-      {
-        test: /css\/registro\.css$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader'
-        ]
-      },
-      {
-        test: /css\/slider\.css$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader'
-        ]
-      }
-    ]
+    ],
   },
   plugins: [
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin({
-      filename: '[name].css'
+      filename: "[name].css",
     }),
     new HtmlWebpackPlugin({
-      template: './index.html',
-      filename: 'index.html',
-      minify: true
-    })
-  ]
-}
+      template: "./index.html",
+      filename: "index.html",
+      minify: true,
+    }),
+    new HtmlWebpackPlugin({
+      template: "./html/acne.html",
+      filename: "./acne.html",
+      minify: true,
+    }),
+
+    new HtmlWebpackPlugin({
+      template: "./html/antioxidantes.html",
+      filename: "./antioxidantes.html",
+      minify: true,
+    }),
+    new HtmlWebpackPlugin({
+      template: "./html/carro.html",
+      filename: "./carro.html",
+      minify: true,
+    }),
+    new HtmlWebpackPlugin({
+      template: "./html/catalogo.html",
+      filename: "./catalogo.html",
+      minify: true,
+    }),
+    new HtmlWebpackPlugin({
+      template: "./html/favorites.html",
+      filename: "./favorites.html",
+      minify: true,
+    }),
+    new HtmlWebpackPlugin({
+      template: "./html/hidratacion.html",
+      filename: "./hidratacion.html",
+      minify: true,
+    }),
+    new HtmlWebpackPlugin({
+      template: "./html/login.html",
+      filename: "./login.html",
+      minify: true,
+    }),
+    new HtmlWebpackPlugin({
+      template: "./html/maquillaje.html",
+      filename: "./maquillaje.html",
+      minify: true,
+    }),
+    new HtmlWebpackPlugin({
+      template: "./html/nosotros.html",
+      filename: "./nosotros.html",
+      minify: true,
+    }),
+
+    new HtmlWebpackPlugin({
+      template: "./html/recomendaciones.html",
+      filename: "./recomendaciones.html",
+      minify: true,
+    }),
+    new HtmlWebpackPlugin({
+      template: "./html/registro.html",
+      filename: "./registro.html",
+      minify: true,
+    }),
+    new HtmlWebpackPlugin({
+      template: "./html/tonicos.html",
+      filename: "./tonicos.html",
+      minify: true,
+    }),
+  ],
+};
