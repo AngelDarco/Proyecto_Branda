08cc74a6f3683a21c8e67728a0b981d94f1e9adc
angeldarco <angel92-eme@hotmail.com>
2024-04-15 01:15:18 -0500

    :wrench: eslint config updated

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,28 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
-  extends: 'standard',
+  extends: "eslint:recommended",
   overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
   ],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
   rules: {
-  }
-}
+    indent: ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+  },
+};
