3d3a456976678939cd357c54ea1f10ada95721ef
angeldarco <angel92-eme@hotmail.com>
2024-04-15 01:26:50 -0500

    :hammer: handlerSpa file refactored

--- js/handlerSpa.js
+++ js/handlerSpa.js
@@ -1,38 +1,39 @@
-export default function handlerSpa (hash) {
-  const container = window.document.getElementById('containerSpa')
-  const frame = container.children[0]
+export default function handlerSpa(hash) {
+  const container = window.document.getElementById("containerSpa");
+
+  const frame = container.children[1];
   switch (hash) {
-    case '#car':
-      frame.setAttribute('src', './html/carro.html')
-      window.location.href = '#containerSpa'
-      break
-    case 'acne':
-      frame.setAttribute('src', './html/acne.html')
-      window.location.href = '#containerSpa'
+    case "#car":
+      frame.setAttribute("src", "./html/carro.html");
+      window.location.href = "#containerSpa";
+      break;
+    case "acne":
+      frame.setAttribute("src", "./html/acne.html");
+      window.location.href = "#containerSpa";
 
-      break
-    case 'antioxidantes':
-      frame.setAttribute('src', './html/antioxidantes.html')
-      window.location.href = '#containerSpa'
+      break;
+    case "antioxidantes":
+      frame.setAttribute("src", "./html/antioxidantes.html");
+      window.location.href = "#containerSpa";
 
-      break
-    case 'hidratacion':
-      frame.setAttribute('src', './html/hidratacion.html')
-      window.location.href = '#containerSpa'
+      break;
+    case "hidratacion":
+      frame.setAttribute("src", "./html/hidratacion.html");
+      window.location.href = "#containerSpa";
 
-      break
-    case 'maquillaje':
-      frame.setAttribute('src', './html/maquillaje.html')
-      window.location.href = '#containerSpa'
+      break;
+    case "maquillaje":
+      frame.setAttribute("src", "./html/maquillaje.html");
+      window.location.href = "#containerSpa";
 
-      break
-    case 'tonicos':
-      frame.setAttribute('src', './html/tonicos.html')
-      window.location.href = '#containerSpa'
+      break;
+    case "tonicos":
+      frame.setAttribute("src", "./html/tonicos.html");
+      window.location.href = "#containerSpa";
 
-      break
+      break;
 
     default:
-      break
+      break;
   }
 }
