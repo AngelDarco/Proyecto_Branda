27e2932f9475a1787605b1b9ffd05f2bfbcebe0f
angeldarco <angel92-eme@hotmail.com>
2024-04-15 01:24:53 -0500

    :hammer: sesion files refactored

--- js/logout.js
+++ js/logout.js
@@ -1,26 +1,28 @@
-export default function logout (loginIcon, logoutIcon, nickname) {
-  logoutIcon.addEventListener('click', () => {
-    fetch('./php/logout.php')
-      .then(res => res.json())
-      .then(data => {
-        if (data === 'Logout') {
-          loginIcon.classList.remove('hide')
-          logoutIcon.classList.add('hide')
-          nickname.innerHTML = 'Invitado'
-          window.localStorage.removeItem('BeautyFaceSession')
+export default function logout(loginIcon, logoutIcon, nickname) {
+  if (loginIcon)
+    logoutIcon.addEventListener("click", () => {
+      fetch("./php/logout.php")
+        .then((res) => res.json())
+        .then((data) => {
+          if (data === "Logout") {
+            loginIcon.classList.remove("hide");
+            logoutIcon.classList.add("hide");
+            if (nickname) nickname.innerHTML = "Invitado";
+            window.localStorage.removeItem("BeautyFaceSession");
 
-          // eslint-disable-next-line
+            // eslint-disable-next-line
             Swal.fire({
-            position: 'center',
-            icon: 'warning',
-            title: 'Session Cerrada',
-            showConfirmButton: false,
-            timer: 1000
-          })
-        }
-      })
-      .catch(err => {
-        console.log(err)
-      })
-  })
+              position: "center",
+              icon: "warning",
+              title: "Session Cerrada",
+              showConfirmButton: false,
+              timer: 1000,
+            });
+            setTimeout(() => {
+              window.location.href = "./index.html";
+            }, 2000);
+          }
+        })
+        .catch((err) => new Error(err));
+    });
 }
--- js/sesion.js
+++ js/sesion.js
@@ -1,6 +1,6 @@
-export default function session (login, logout, nickname, session) {
-  if (!session) return
-  nickname.innerHTML = session
-  login.classList.add('hide')
-  logout.classList.remove('hide')
+export default function session(login, logout, nickname, session) {
+  if (!session) return;
+  nickname.innerHTML = session;
+  login.classList.add("hide");
+  logout.classList.remove("hide");
 }
